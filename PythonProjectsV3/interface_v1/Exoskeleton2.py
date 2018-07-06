aad350a789bd8a93d5356cf02b3a59e34503f247
JiangYingyan <790568256@qq.com>
2018-07-06 15:07:51 +0800

    添加控制系外骨骼

--- PythonProjectsV3/interface_v1/Exoskeleton2.py
+++ PythonProjectsV3/interface_v1/Exoskeleton2.py
@@ -1,49 +1,56 @@
 # -*- coding: utf-8 -*-
 
-#香港理工机械手控制
-import serial
+#控制系外骨骼控制，各个函数作用参考Exoskeleton.py
+import socket
+
 class Exoskeleton:
     def __int__(self):
         print('link to exoskeleton')
-        self.EpochCommand = ['rest', 'rest', 'rest']
-        self.LastCommand = 'rest'
-        self.CommandNumber = 0
-    #连接串口，输入串口号和波特率
-    def LinkToExoskleton(self, comNum, baudRate):
-        print('Trying to Connect to COM')
-        com_name = 'COM' + str(comNum)
-        self.COM = None
+#为机械手建立服务端，等待机械臂连接
+    def LinkToExoskleton(self, ip, port):
+        ip = "localhost"
+        port = 12234
+        self.ESocket1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_address = (ip, port)
+        self.ESocket1.bind(server_address)
+        print('wait for exoskeleton connect,starting listen on localhost, 12234')
+        self.connected = False
         try:
-            self.COM = serial.Serial(com_name, baudRate)
-            print('Connect to COM successfully')
-        except Exception as e:
-            print('Open serial failed.' + str(e))
-        print('link to exoskeleton')
-        self.EpochCommand = ['rest', 'rest', 'rest']
-        self.LastCommand = 'rest'
-        self.CommandNumber = 0
+            self.ESocket1.listen(1)
+        except socket.error as e:
+            print("fail to listen on port " )
+        addr = None
+        while(not addr):
+            self.ESocket,addr = self.ESocket1.accept()
+        self.connected = True
+        print("connect to exoskeleton successfully")
+        self.CommandNumber = None
 
-    #初始化 输入外骨骼的运动速度，运动起始角度和结束角度
     def SendHeadCommandToCom(self, speed, startangle, endangle):
-        Command = 'C' + str(speed) + 'A'+str(startangle) + 'B'+str(endangle)
-        self.COM.write(Command.encode())
-        print('Initial Setting Successfully')
-#给串口发送动指令
+            print('Initial Setting Successfully')
+
     def ExoskeletonMove(self):
         print('move')
-        self.COM.write('1'.encode())
-#给串口发送停止指令
+        self.ESocket.sendall('move'.encode())
+
+
     def ExoskeletonStop(self):
         print('stop')
-        self.COM.write('0'.encode())
-#按每个tiral执行命令
+        self.ESocket.sendall('rest'.encode())
+
+
     def StrategyTiral(self,command):
         if(command == 'move'):
             self.ExoskeletonMove()
         if(command == 'rest'):
             self.ExoskeletonStop()
-#按每个epoch执行命令
+
     def StrategyEpoch(self,command):
+        if(not self.CommandNumber):
+            self.CommandNumber = 0
+            self.LastCommand = 'rest'
+            self.EpochCommand = ['rest', 'rest', 'rest']
+
         self.EpochCommand[self.CommandNumber % len(self.EpochCommand)] = command
         print(self.EpochCommand)
         self.CommandNumber = self.CommandNumber + 1
@@ -55,28 +62,24 @@ class Exoskeleton:
         elif(restnumber == len(self.EpochCommand) and self.LastCommand == 'move'):
             self.ExoskeletonStop()
             self.LastCommand = 'rest'
-#关闭串口
+
     def Disconnected(self):
         self.ExoskeletonStop()
-        self.COM.close()
-
+        self.ESocket.close()
 
 if __name__ == "__main__":
     # 测试外骨骼指令策略(模拟Epoch分类结果)
     import time
     ES = Exoskeleton()
-    comNum = 17
-    baudRate = 9600
+    comNum = 'localhost'
+    baudRate = 5566
     ES.LinkToExoskleton(comNum, baudRate)
+    time.sleep(2)
     ES.SendHeadCommandToCom(30, 1700, 2200)
     testlist = ['move', 'move', 'move', 'move', 'rest', 'rest','rest','rest','move', 'move', 'move']
     for i in range(len(testlist)):
         ES.StrategyEpoch(testlist[i])
-        time.sleep(1.5)
+        time.sleep(2)
     ES.ExoskeletonStop()
     time.sleep(2)
     ES.Disconnected()
-
-
-
-
